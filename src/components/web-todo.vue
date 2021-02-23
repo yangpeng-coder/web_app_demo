02675cf625fcfc7b6b7c940a1dfd7d4a184b92aa
unknown <yp.asolu@gmail.com>
2021-02-23 10:34:57 +0900

    web-todo

---
+++ src/components/web-todo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <input type="text" v-model="inputValue" @keyup.enter="addItem(inputValue)">
+        <table>
+            <tr v-for="(item, index) in items" :key="index">
+                <td>{{ index + 1 }}{{ item }}
+                    <button type="button" @click="delItem(index)">x</button>
+                </td>
+            </tr>
+            <tr v-show="items.length !== 0">
+                <td>total: {{ items.length }}
+                    <button type="button" @click="initItem">clean all!</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "web-todo",
+    data() {
+        return {
+            items: [],
+            inputValue: '',
+        }
+    },
+    methods: {
+        initItem: function () {
+            this.items = [];
+        },
+        addItem: function (value) {
+            this.items.push(value);
+            this.inputValue = '';
+        },
+        delItem: function (index) {
+            this.items.splice(index, 1);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

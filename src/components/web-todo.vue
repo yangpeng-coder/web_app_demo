6ec7e3cd6eb4b93bd46c4bc4d96c63f6510c7bb6
yangpeng-coder <yp.asolu@gmail.com>
2021-02-23 20:26:55 +0900

    add features

--- src/components/web-todo.vue
+++ src/components/web-todo.vue
@@ -13,16 +13,26 @@
                 </td>
             </tr>
         </table>
+        <button type="button" @click="getJoke">get joke</button>
+        <p>{{jokeContent}}</p>
+        <input type="text" @keyup.enter="searchWeather(city)" v-model="city">
+        <button type="button" @click="searchWeather(city)">Search</button>
+        <a href="#" v-for="(c, index) in defaultCitys" :key="index" @click="searchWeather(c)">{{c}}</a>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "web-todo",
     data() {
         return {
             items: [],
             inputValue: '',
+            jokeContent: '',
+            city: '',
+            defaultCitys: ['北京', '上海', '深圳', '广州'],
         }
     },
     methods: {
@@ -30,11 +40,33 @@ export default {
             this.items = [];
         },
         addItem: function (value) {
-            this.items.push(value);
-            this.inputValue = '';
+            if (value !== '') {
+                this.items.push(value);
+                this.inputValue = '';
+            }
         },
         delItem: function (index) {
             this.items.splice(index, 1);
+        },
+        getJoke: function () {
+            axios.get('https://autumnfish.cn/api/joke')
+                .then((response) => {
+                    this.jokeContent = response.data;
+                }, (err) => {
+                    console.log(err);
+                });
+        },
+        searchWeather: function (c) {
+            if (c === '') { return; }
+            axios.get('http://wthrcdn.etouch.cn/weather_mini?city=' + c)
+            .then((response) => {
+                console.log(response.data);
+                if (response.data.status === 1002) {
+                    alert(response.data.desc)
+                }
+            }, (err) => {
+                console.log(err);
+            })
         }
     }
 }
